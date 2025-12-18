86c126f71fbaeebf9b30e02e9fce27a8e141d74f
Pedro Liberal <phgui.subscription@proton.me>
2025-12-17 21:32:51 -0300

    Fix RPM Fusion

--- fpi.sh
+++ fpi.sh
@@ -214,8 +214,7 @@ function enable_RPMFusion() {
     prompt -bd "Enabling RPM Fusion default repositories..."
     prompt -bd "This is include free and nonfree releases"
 
-    ensure_sudo;
-
+    ensure_sudo
     dnf install -y \
         https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
         https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
