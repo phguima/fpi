c7959f1018c7fc4328e2321031f94ddb7761c2a2
Pedro Liberal <phgui.subscription@proton.me>
2025-12-17 21:29:35 -0300

    Flatpak e RPMFusion functions

--- fpi.sh
+++ fpi.sh
@@ -28,18 +28,20 @@ b_CWAR="\033[1;33m"                            		        # bold warning color
 # Exit immediately if a command fails
 set -o errexit
 
-# Variables
+# Global constants
 ROOT_UID=0
-DNF_SELECT=()
-FLATPAK_SELECT=()
+
+# Global variables
+dnf_select=()
+flatpak_select=()
 
 # Config files options
-copr_file_options=()
-microsoftKeys_file_options=()
+copr_options=()
+ms_options=()
 
 # User selected options
-copr_install_options=()
-microsoftKeys_install_options=()
+#copr_install_options=()
+#microsoftKeys_install_options=()
 
 
 #######################################
@@ -50,71 +52,175 @@ microsoftKeys_install_options=()
 function prompt () {
 	case ${1} in
         "-d"|"--default")
-			echo -e "${b_CDEF}${@/-d/}${CDEF}";;            # print success message
+			echo -e "${CDEF}${@/-d/}${CDEF}";;          # print default message
+        "-bd"|"--default-bold")
+			echo -e "${b_CDEF}${@/-bd/}${CDEF}";;       # print bold default message
 		"-s"|"--success")
-			echo -e "${b_CGSC}${@/-s/}${CDEF}";;            # print success message
+			echo -e "${CGSC}${@/-s/}${CDEF}";;          # print success message
+        "-bs"|"--success-bold")
+			echo -e "${b_CGSC}${@/-bs/}${CDEF}";;       # print bold success message
 		"-e"|"--error")
-			echo -e "${b_CRER}${@/-e/}${CDEF}";;            # print error message
+			echo -e "${CRER}${@/-e/}${CDEF}";;          # print error message
+        "-be"|"--error-bold")
+			echo -e "${b_CRER}${@/-be/}${CDEF}";;       # print bold error message
 		"-w"|"--warning")
-			echo -e "${b_CWAR}${@/-w/}${CDEF}";;            # print warning message
+			echo -e "${CWAR}${@/-w/}${CDEF}";;          # print warning message
+        "-bw"|"--warning-bold")
+			echo -e "${b_CWAR}${@/-bw/}${CDEF}";;       # print bold warning message
 		"-i"|"--info")
-			echo -e "${b_CCIN}${@/-i/}${CDEF}";;            # print info message
+			echo -e "${CCIN}${@/-i/}${CDEF}";;          # print info message
+        "-bi"|"--info-bold")
+			echo -e "${b_CCIN}${@/-bi/}${CDEF}";;       # print boldinfo message
 		*)
 			echo -e "$@"
 		;;
 	 esac
 }
 
-# Function to check if the script is run as root
-function check_root() {
+: '
+# Check if the script is run as root
+function sudo() {
     if [ "$UID" -ne "$ROOT_UID" ]; then
-        echo "This script must be run as root. Please run 'sudo ./fpi.sh'"
+        prompt -be "This script must be run as root. Please run 'sudo ./fpi.sh'"
         exit 1
     fi
 }
+'
 
-# Function to check and refresh sudo cache
-function check_sudo_cache() {
-    # Checks if the sudo timestamp is valid without prompting for a password
-    while ! sudo -n true 2> /dev/null; do
-        echo -e "Sudo credentials are required to proceed."
-
-        # Force a password prompt
-        if ! sudo true; then
-            echo "Authentication failed. Trying again..."
-        fi
-    done
+# Active sudo silent check
+function sudo_check() {
+    # The last command return status is the function return
+    sudo -n true 2> /dev/null
 }
 
+# Check and refresh sudo cache
+function ensure_sudo() {
+    # Checks if the sudo timestamp is valid without prompting for a password
+    while ! sudo_check; do
+        prompt -be "Sudo credentials are required to proceed."
+        
+        # Ask the password to the users (3 attempts)
+        sudo -v
+        
+        # Check for a success sudo -v
+        # Loop while $? is not igual to 0
+        if [ $? -ne 0 ]; then
+             echo "Authentication failed. Trying again (or CRTL+C to exit script)..."
+        fi
+    done
+    
+    echo "Sudo activated..."
+}
+
+: '
 # Function to display usage information
 function usage() {
     echo "Usage: $0 [OPTION]..."
     echo "            "
     echo "OPTIONS (assumes '-a' if no parameters is informed):"
-    echo "  -a, --all          run all update system (dnf, flatpal and fwupdmgr) [Default]"
+    echo "  -a, --all          run all update system (dnf, flatpak and fwupdmgr) [Default]"
     echo "  -d, --dnf          run 'dnf upgrade --refresh'"
     echo "  -f, --flatpak      run 'flatpak update'"
     echo "  -x, --firmware     run firmware update commands (fwupdmgr)"
     echo "  -h, --help         Show this help"
     echo ""
 }
+'
 
-function show_menu() {
+: '
+Estrutura do menu
+
+|- System setup
+|--- Configuration
+        |------ Enable flathub
+|------ Enable RPMFusion
+|------ Optimise DNF speed
+|--- System update
+|------ DNF
+|------ Flatpak
+|------ Firmware
+|- Software setup
+|--- Install Oh-My-ZSH
+|--- Install DNF softwares
+|--- Install flatpak softwares
+|--- Install microsoft softwares
+|--- Install extras (fonts & codecs)
+|- Hardware setup
+|--- Install VA-API driver for Intel GPUs
+|--- Install VA-API and VDPAU drivers for AMD GPUs
+|--- Install NVIDIA proprietary driver (signed)
+|- Tweaks
+|--- Set hostname
+'
+
+# Header to be used with all menus
+function menu_header() {
     clear
-    prompt -s "========================================"
-    prompt -s "    Fedora Post Installation Script     "
-    prompt -s "         Author: Pedro Liberal          "
-    prompt -s "========================================"
-    prompt -s ""
-    prompt -w "Menu (Choose one or more options separated by comman)"
-    prompt -d " 1. System upgrade (dnf | flatpak | firmware)"
-    prompt -d " 2. Base install (copr repositories, packages and better fonts)"
-    prompt -d " 3. Microsoft packages"
-    prompt -d " 4. Flatpak packages install"
-    prompt -d " 5. nVidia install (Requires manual intervention)"
-    prompt -d " 0. All steps above"
+    prompt -bs "+======================================+"
+    prompt -bs "        Fedora Post Installation        "
+    prompt -bs "            by Pedro Liberal            "
+    prompt -bs "                  v1.0                  "
+    prompt -bs "+======================================+"
+    prompt -bs ""
+}
+
+# fpi main menu
+function menu_main() {
+    menu_header
+    prompt -bw "Main Menu"
+    prompt -d " 1. System setup"
+    prompt -d " 2. Software setup"
+    prompt -d " 3. Hardware setup"
+    prompt -d " 4. Tweaks"
     prompt -d " -"
-    prompt -d " Q. Quit"
+    prompt -d " 0. Quit"
+    prompt -d ""
+}
+
+# System menu
+function menu_system() {
+    menu_header
+    prompt -bw "System Menu"
+    prompt -bi "- Configuration"
+    prompt -d "  1. Enable flathub"
+    prompt -d "  2. Enable RPMFusion"
+    prompt -d "  3. Optimise DNF speed"
+    prompt -bi "- Updates"
+    prompt -d "  4. DNF"
+    prompt -d "  5. Flatpak"
+    prompt -d "  6. Firmware"
+    prompt -d "  -"
+    prompt -d "  0. Quit"
+    prompt -d ""
+}
+
+# Enables flatpak remotes if not exist
+function enable_flatpak() {
+    prompt -bd ""
+    prompt -bd "Trying to enable flatpak remotes if not exist..."
+    prompt -bd "User password will be prompted using GUI"
+    sleep 1 
+
+    if sudo_check; then
+        runuser -u $SUDO_USER -- flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    else
+        flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    fi
+}
+
+#
+function enable_RPMFusion() {
+    prompt -bd ""
+    prompt -bd "Enabling RPM Fusion default repositories..."
+    prompt -bd "This is include free and nonfree releases"
+
+    ensure_sudo;
+
+    dnf install -y \
+        https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+        https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    dnf upgrade --refresh -y
+    dnf group upgrade -y core
 }
 
 # Function to show nVidia installation warning
@@ -179,6 +285,46 @@ function nvidia_reboot() {
 }
 
 # Main script execution
-check_root
-show_menu
+#sudo
+
+while true; do
+    menu_main
+    read -p " Option: " option
+
+    case ${option} in
+        1)
+            menu_system
+            read -p " Option: " option
+
+            case ${option} in
+                1)
+                    enable_flatpak
+                    ;;
+                2)
+                    ;;
+                3)
+                    ;;
+                4)
+                    ;;
+                5)
+                    ;;
+                6)
+                    ;;
+                0)
+                    ;;
+                *)
+                    ;;
+            esac
+            ;;
+        0)
+            prompt -bs ">>>   Thanks for use Fedora Post Installation Script   <<<"
+            exit 0
+            ;;
+        *)
+            exit 1
+            ;;
+    esac
+done
+
+
 
